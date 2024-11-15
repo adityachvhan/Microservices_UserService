20ff69fd53401b4b075459d95b88dba0ff970890
aditya chavhan <adityachavhan11@gmail.com>
2024-11-15 14:25:16 +0530

    UserController is done

--- src/main/java/com/corejavahub/controller/UserController.java
+++ src/main/java/com/corejavahub/controller/UserController.java
@@ -3,6 +3,8 @@ package com.corejavahub.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,12 @@ public class UserController {
 		User createUser = userService.saveUser(user);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(createUser);
+	}
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
+
+		User user = userService.getUser(userId);
+		return ResponseEntity.ok(user);
 	}
 }
